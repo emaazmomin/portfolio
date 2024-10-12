5c93b95379b830fe27275ea4ccab453a2432e1fc
Emaaz Momin <mominemaz@gmail.com>
2024-10-12 15:41:26 +0530

    emaz

--- src/AppProperties.js
+++ src/AppProperties.js
@@ -1,4 +1,4 @@
 export const AppProperties={
-// loca:"https://portfolio-server-mu-gray.vercel.app"
-loca:"http://localhost:5000"
+loca:"https://portfolio-server-mu-gray.vercel.app"
+// loca:"http://localhost:5000"
 }
